b525fe009d1876ca7a3a3c0c97c80f9337254f98
Nudaa <nudaadeshmukh@gmail.com>
2026-02-22 20:01:14 +0530

    Initial Spring Boot backend setup

---
+++ src/test/java/com/example/SABLE/SableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SABLE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
